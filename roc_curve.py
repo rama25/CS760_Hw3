1970ab67a4617324dd1f2408779f3c8e4c6aac1f
Ramapriya <ramapriya288@gmail.com>
2023-02-25 19:31:50 -0600

    Update roc_curve.py

--- roc_curve.py
+++ roc_curve.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("homework3/data/Q5.txt", sep=" ", header=None, names=["y", "c"])
+data = pd.read_csv("Q5.txt", sep=" ", header=None, names=["y", "c"])
 
 n_pos = sum(data.y)
 n_neg = len(data.y) - n_pos
@@ -26,7 +26,7 @@ roc_y.append(1)
 plt.plot(roc_x, roc_y)
 plt.scatter(roc_x, roc_y)
 plt.title("ROC Curve")
-plt.savefig("homework3/figs/q5_roc_curve.png")
+plt.savefig("figs/q5_roc_curve.png")
 
 if __name__ == '__main__':
     pass
